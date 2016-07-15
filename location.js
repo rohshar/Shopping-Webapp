243d6b7c083711d527888216c6c4f4053410e467
Rohan Sharan <sharanrohan@berkeley.edu>
2016-07-15 14:18:35 -0400

    just a very barebones start which gets the input of the user and displays it, will try to work with google API

---
+++ location.js
@@ -0,0 +1,5 @@
+function displayInput(id, change) {
+    var text=document.getElementById(id).value;
+    $(change).html(text);
+}
+
---
+++ test01.cpp
@@ -0,0 +1,33 @@
+using namespace std;
+#include <iostream>
+
+int main()
+{
+   int a;                    // a is an integer variable
+   char s [100];             // s points to a string of max 99 characters
+
+   cout << "This is a sample program." << endl;
+
+   cout << endl;             // Just a line feed (end of line)
+
+   cout << "Type your age : ";
+   cin >> a;
+
+   cout << "Type your name: ";
+   cin >> s;
+
+   cout << endl;
+
+   cout << "Hello " << s << " you're " << a << " old." << endl;
+   cout << endl << endl << "Bye!" << endl;
+
+   return 0;
+}
+
+
+
+
+
+
+//My Google Places API key (Server) = AIzaSyAfEikMyjUP2Lg1Y4B1FeiFVIMG1Q4Vynk
+//My Google Javascript API key (Browser) = AIzaSyA-3aU8cc1ODOsIAAqyvHE4Qt4wq0oRQ0A  

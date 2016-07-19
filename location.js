eee3af354f14346581705f58a86c093b324bd788
Rohan Sharan <sharanrohan@berkeley.edu>
2016-07-19 17:31:37 -0400

    fixed slight things

--- location.js
+++ location.js
@@ -11,6 +11,30 @@ window.onload = function() {
 
 
 
+function testLocation() {
+	console.log(google.loader.ClientLocation);
+	if ((google.loader) && (google.loader.ClientLocation)) {
+        var latitude = google.loader.ClientLocation.latitude;
+        var longitude = google.loader.ClientLocation.longitude;
+        var city = google.loader.ClientLocation.address.city;
+        var country = google.loader.ClientLocation.address.country;
+        var country_code = google.loader.ClientLocation.address.country_code;
+        var region = google.loader.ClientLocation.address.region;
+
+        var text = 'Your Location<br /><br />Latitude: ' + latitude + '<br />Longitude: ' + longitude + '<br />City: ' + city + '<br />Country: ' + country + '<br />Country Code: ' + country_code + '<br />Region: ' + region;
+
+    } else {
+
+        var text = 'Google was not able to detect your location';
+
+    }
+
+    document.write(text);
+}
+
+
+
+
 
 function displayInput(input, change) {
     var text=document.getElementById(input).value;

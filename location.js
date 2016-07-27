bef00c2556a92d0a01345839c756ba5505069a6a
Rohan Sharan <sharanrohan@berkeley.edu>
2016-07-27 16:40:56 -0400

    got markets placed on relevant locations

--- location.js
+++ location.js
@@ -14,14 +14,20 @@ window.onload = function() {
 };
 
 
-
+var location_map;
 function initialize() {
   if (document.getElementById('startLat').innerHTML != "This is your latitude") {
     var my_place = new google.maps.LatLng(document.getElementById('startLat').innerHTML, document.getElementById('startLon').innerHTML);
 
-    var location_map = new google.maps.Map(document.getElementById('map'), {
+    location_map = new google.maps.Map(document.getElementById('map'), {
         center: my_place,
         zoom: 15
+    });
+
+    var centerMarker = new google.maps.Marker({
+      position: my_place,
+      map: location_map,
+        icon: 'GoogleMapsMarkers/red_MarkerA.png'
     });
 
     var request = {
@@ -35,8 +41,8 @@ function initialize() {
     service.textSearch(request, callback);
   }
   else {
-    document.getElementById('startLat').innerHTML = "Let your latitude load!"
-    document.getElementById('startLon').innerHTML = "Let your longitude load!"
+    document.getElementById('startLat').innerHTML = "Let your latitude load!";
+    document.getElementById('startLon').innerHTML = "Let your longitude load!";
   }
 }
 
@@ -45,10 +51,16 @@ function callback(results, status) {
   if (status == google.maps.places.PlacesServiceStatus.OK) {
     for (var i = 0; i < results.length; i++) {
       var place = results[i];
-      console.log(place)
-      places = places + "   " + place['id']
-      $("#allPlaces").html(places)
+      //console.log(place)
+      places = places + "   " + place['id'];
+      $("#allPlaces").html(places);
 
+      console.log(place.geometry.location)
+      var marker = new google.maps.Marker({
+        position: place.geometry.location,
+        map: location_map,
+        icon: 'GoogleMapsMarkers/blue_MarkerA.png'
+      });
 
       //createMarker(results[i]);
     }

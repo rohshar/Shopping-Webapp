50bfb0980e6b48159545b31405706b8a58dbff59
Rohan Sharan <sharanrohan@berkeley.edu>
2016-08-29 22:53:58 -0700

    debugging

--- location.js
+++ location.js
@@ -1,4 +1,6 @@
-
+var latitude = null;
+var longitude = null;
+var usingCurrentLocation = true;
 
 window.onload = function() {
   $("#accordion").hide();
@@ -15,28 +17,7 @@ window.onload = function() {
     latitude = startPos.coords.latitude;
     longitude = startPos.coords.longitude;
   };
-  //navigator.geolocation.getCurrentPosition(geoSuccess);
-  jQuery.post( "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyDCa1LUe1vOczX1hO_iGYgyo8p_jYuGOPU", function(success) {
-    latitude = success.location.lat;
-    longitude = success.location.lng;
-  })
-  .fail(function(err) {
-    alert("FAILED");
-  });
-
-var apiGeolocationSuccess = function(position) {
-  alert("API geolocation success!\n\nlat = " + position.coords.latitude + "\nlng = " + position.coords.longitude);
-};
-
-var tryAPIGeolocation = function() {
-  jQuery.post( "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyDCa1LUe1vOczX1hO_iGYgyo8p_jYuGOPU", function(success) {
-    apiGeolocationSuccess({coords: {latitude: success.location.lat, longitude: success.location.lng}});
-  })
-  .fail(function(err) {
-    alert("API Geolocation error! \n\n"+err);
-  });
-
-
+  navigator.geolocation.getCurrentPosition(geoSuccess);
   var mapDiv = document.getElementById('map');
   location_map = new google.maps.Map(mapDiv, {
       center: {lat: 0, lng: 0},
@@ -477,6 +458,26 @@ function directions(mode) {
         directionsService1.route({
             origin: {lat: lat, lng: lon},
             destination: {lat: chosenPlace.geometry.location.lat(), lng: chosenPlace.geometry.location.lng()},
+            travelMode: google.maps.TravelMode.TRANSIT
+        }, function(response, status) {
+            if (status == 'OK') {
+                directionsDisplay.setDirections(response);
+                var table = document.getElementById("directionTable");
+                for (var i=0; i<response.routes[0].legs[0].steps.length; i++) {
+                    var row = table.insertRow(i);
+                    var cell = row.insertCell(0);
+                    cell.innerHTML = response.routes[0].legs[0].steps[i].instructions;
+                }
+
+            } else {
+                window.alert('Directions request failed due to ' + status);
+            }
+        });
+    }
+}
+
+
+g: chosenPlace.geometry.location.lng()},
             travelMode: google.maps.TravelMode.TRANSIT
         }, function(response, status) {
             if (status == 'OK') {
